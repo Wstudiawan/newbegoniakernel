6d3f1faeeae12927eb590208d5dfc0bd113a9773
Wahyu Studiawan <manusiasampah7@gmail.com>
2022-10-14 12:06:46 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -49,7 +49,7 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 beryllium_defconfig
+    make O=out ARCH=arm64 begonia_user_defconfig
     make -j$(nproc --all) O=out \
                     ARCH=arm64 \
                     CC=clang \
